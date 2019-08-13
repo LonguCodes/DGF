038f0367a0a447b3e27808ce91d1838d51882780
maciej <lyskawinski.maciej@gmail.com>
2019-08-13 13:36:21 +0200

    0.0.1.post1
    Fixed path error in BearerAuthenticator example in README

--- DGF/builtins/__init__.py
+++ DGF/builtins/__init__.py
@@ -1,3 +1,3 @@
 from .permissions import ReadOnlyPermission
 from .links import AuthLink, ExecuteLink, FetchLink, ObjectPermissionLink, RequestPermissionLink
-from .authenticators import ModelAuthenticator, BackendAuthenticator
+from .authenticators import ModelAuthenticator, BackendAuthenticator #TODO: Add BearerAuthenticator to __init__
